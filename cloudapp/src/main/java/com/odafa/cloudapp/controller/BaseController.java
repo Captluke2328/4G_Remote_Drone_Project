1988fbaca7f19a9ec997b83fb501f20f003469cb
Captluke <lukasjohnny2328@gmail.com>
2022-06-22 17:34:09 +0800

    added map and marking

--- cloudapp/src/main/java/com/odafa/cloudapp/controller/BaseController.java
+++ cloudapp/src/main/java/com/odafa/cloudapp/controller/BaseController.java
@@ -3,13 +3,18 @@ package com.odafa.cloudapp.controller;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.google.gson.Gson;
 import com.odafa.cloudapp.configuration.ConfigReader;
+import com.odafa.cloudapp.dto.DroneInfo;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +26,21 @@ public class BaseController {
     
     private final ConfigReader configurations;
 
+    @ResponseBody
+    @GetMapping("/updateSystemInfo")
+    public String updateSysteminfo () {
+
+        final Gson gson = new Gson();
+        final List<DroneInfo> drones = new ArrayList<>();
+        
+        DroneInfo dto1 = new DroneInfo("1", 1.553504, 110.359291, 0.0468, 7.1, 12.3, "ONLINE");
+        DroneInfo dto2 = new DroneInfo("2", 1.553504, 110.359291, 0.0468, 3, 21, "ONLINE");
+        drones.add(dto1);
+        drones.add(dto2);
+
+        return gson.toJson(drones);
+    }
+
     @GetMapping("/")
     public String indexPage(Model model){
 
@@ -29,7 +49,7 @@ public class BaseController {
         model.addAttribute("defaultAltitude", configurations.getDefaultAltitude());
         model.addAttribute("videoEndpoint", configurations.getVideoWsEndpoint());
         
-        log.debug("Index Page Opened");
+        //log.debug("Index Page Opened");
         
         return "index";
     }
@@ -59,6 +79,8 @@ public class BaseController {
 		// }
         // log.debug(ip);
         String ip = "10.60.215.193";
+        //String ip = "192.168.8.115";
+
 		return ip;
 	}
 }
